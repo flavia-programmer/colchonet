2a99bb13379106438b1e8a10f44f674c0d19b260
Flávia Amaral <flavia.programadora@gmail.com>
2014-12-24 15:57:56 -0200

    até pag 169

---
+++ app/models/room.rb
@@ -0,0 +1,8 @@
+class Room < ActiveRecord::Base
+  	attr_accessible :description, :location, :title
+  	validates_presence_of :description, :location, :title
+  	validates_length_of :description, :minimum => 30, :allow_blank => false
+  	def complete_name
+		"#{title}, #{location}"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colchonet::Application.config.secret_token = '9d1b1ac1414bca2d7859943d3c38a7ef2ddeb45121c2f769dfb44c82cb29c45058ddf11abbeb473888c550330d488226ec742d69526b0f271ab108f6228120e7'
