65960746f6dac39696e60ae8580abc1b8fd4ea4b
Flávia Amaral <flavia.programadora@gmail.com>
2015-01-02 15:39:58 -0200

    add gem :letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Colchonet::Application.configure do
   }
 
   config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.smtp_settings = {
     address: "localhost",
     port: 1025
