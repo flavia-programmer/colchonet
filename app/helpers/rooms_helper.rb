f27fcb2ce25c5ea663b19d639fa2d697e1754a0b
Flávia Amaral <flavia.programadora@gmail.com>
2015-01-05 17:57:35 -0200

    implementacao do capitulo 11.6

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,8 +1,5 @@
 module RoomsHelper
-	def error_tag(model, attribute)
-		if model.errors.has_key? attribute
-			content_tag :div, model.errors[attribute].first,
-			:class => 'error_message'
-		end
+	def belongs_to_user(room)
+		user_signed_in? && room.user == current_user
 	end
 end
